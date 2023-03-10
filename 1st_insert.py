42efe7bea2560da3c8c57c2bb5a20c926121da41
Pavel Bugakov <pashabugakov@yandex.ru>
2023-03-10 21:51:16 +0300

    new command find

--- 1st_insert.py
+++ 1st_insert.py
@@ -5,18 +5,35 @@
 # Вывод: 0
 # Ввод: haystack = "leetcode", needle = "leeto"
 # Вывод: -1
+import timeit
 
+
+x = 0
 print('Введите стог сена')
 haystack = input()
 len_haystack = len(haystack)
 print('Введите иглу')
 needle = input()
 len_needle = len(needle)
+
+print('Первый способ')
+start_time = timeit.default_timer()
 for i in range(len_haystack):
     if haystack[i] == needle[0]:
         index = i
-        break
-if needle == haystack[index:len_needle+index]:
-    print(index)
-else:
-    print(-1)
+        if needle == haystack[index:index+len_needle:]:
+            punch = 1
+            print(index)
+            break
+        else:
+            continue
+if punch != 1:
+    print('-1')
+print(timeit.default_timer() - start_time)
+
+
+print('Второй способ')
+start_time = timeit.default_timer()
+index = haystack.find(needle)
+print(index)
+print(timeit.default_timer() - start_time)
